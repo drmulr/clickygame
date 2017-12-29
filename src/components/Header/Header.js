7a88dc87218c760c8c04b144a2465ba448b625e4
Daniel Muller <29293369+drmulr@users.noreply.github.com>
2017-12-29 17:38:44 -0500

    Header text spacing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = props => (
   <div>
     <div className="navbar navbar-expand-lg navbar-light bg-dark fixed-top" >
       <h2 className="navbar brand text-light">Clicky Game</h2>
-      <h3 className="nav-link text-light text-right">Current Score: <span>{props.currentScore}</span></h3>
+      <h3 className="nav-link text-light text-right">Your Score: <span>{props.currentScore}</span></h3>
       <h3 className="nav-link text-light text-right">High Score: <span>{props.highScore}</span></h3>
     </div>
   </div>
