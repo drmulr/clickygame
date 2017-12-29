3091f1f60c49ce938f3e71d048160b5dc0858f01
Daniel Muller <29293369+drmulr@users.noreply.github.com>
2017-12-29 17:19:26 -0500

    CSS Jumbo Top, and Header Height

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,11 @@ import './Header.css';
 const Header = props => (
 
   <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top" >
+    <div className="navbar navbar-expand-lg navbar-light bg-dark fixed-top" >
       <h2 className="navbar brand text-light">Clicky Game</h2>
       <h3 className="nav-link text-light text-right">Current Score: <span>{props.currentScore}</span></h3>
       <h3 className="nav-link text-light text-right">High Score: <span>{props.highScore}</span></h3>
-    </nav>
+    </div>
   </div>
 
 );
--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -4,7 +4,7 @@ import './Jumbo.css';
 const Jumbo = props => (
 
   <div className="jumbotron">
-    <h1 className="display-3 text-center">Click it!</h1>
+    <h1 className="display-3 text-center">Ready to play?</h1>
     <h3 className="text-center">Don't be a hero. Click each once, but never more than once...or else.</h3>
   </div>
 
