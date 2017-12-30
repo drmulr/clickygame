971fa4f085db3813581ba651dd9bbd0fc0b215c9
Daniel Muller <29293369+drmulr@users.noreply.github.com>
2017-12-30 14:02:15 -0500

    Hover animation on targets

--- src/components/Targets/Targets.js
+++ src/components/Targets/Targets.js
@@ -6,7 +6,7 @@ import './Targets.css';
 const Targets = props => (
 
   <div onClick={() => props.alreadySelected(props.id)} className="card">
-    <div className="img-container">
+    <div className="img-container target">
       <Overdrive id={props.id}>
         <img alt={props.name} src={props.image} />
       </Overdrive>
